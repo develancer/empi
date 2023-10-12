aa0689f24d6d9ef25451f8ebc0ca768b732c2bc5
Piotr Różański <piotr@develancer.pl>
2023-10-12 23:14:29 +0200

    Better support for MacOS in build-standalone.sh

--- build-standalone.sh
+++ build-standalone.sh
@@ -3,13 +3,21 @@ set -o errexit
 DIR=`mktemp -d`
 trap "rm -rf $DIR" 0 2
 
-VER=empi-`git describe --tags --exact-match`-lin64
+OS=`uname -s | tr '[A-Z]' '[a-z]'`
+if [ "$OS" = darwin ] ; then
+  OS=macos
+  STANDALONE=FALSE
+else
+  STANDALONE=TRUE
+fi
+
+VER=empi-`git describe --tags --exact-match`-$OS
 mkdir -p $DIR/$VER
 cp README.md LICENCE $DIR/$VER
-cmake -DSTANDALONE=TRUE -DWITH_CUDA=FALSE -S. -B$DIR
+cmake -DSTANDALONE=$STANDALONE -DWITH_CUDA=FALSE -S. -B$DIR
 ( cd $DIR \
   && make -j8 empi \
-  && mv empi $VER/empi-lin64 \
+  && mv empi $VER/empi-$OS \
   && zip -9r $VER.zip $VER/
 )
 mv $DIR/$VER.zip .
